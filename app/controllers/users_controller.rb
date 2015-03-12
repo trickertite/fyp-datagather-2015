2a5e7efd0f6115179fbd9633d9a43e9859ffa479
trickertite <trickertite@gmail.com>
2015-03-12 10:57:57 +0200

    users form set

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,17 @@ class UsersController < ApplicationController
 	def index
 
 	end
+
+	def new
+	end
+
+	def create
+		puts '###############################################'
+		puts 'hello world'
+		redirect_to users_path
+	end
+
+	def show
+
+	end
 end
