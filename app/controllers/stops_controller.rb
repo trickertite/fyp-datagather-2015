48f3a68a40c429ef4aefd3f9dbdcaf9793ab0e27
trickertite <trickertite@gmail.com>
2015-03-17 21:59:48 +0200

    Stop record update now

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -11,16 +11,15 @@ class StopsController < ApplicationController
 	def update
 		@user = User.find(params[:user_id])
 
-		if @user.stops.update(stop_params)
+		if @user.stops.update(params[:id], stop_params)
 			render json: @user.locations.count
 		else
-			#render :json => {'error': 'cannot save location into database'}
-			render nothing: true
+			render :json => {'error': 'cannot save location into database'}
 		end
 	end
 
 	private
 	def stop_params
-		params.require(:stop).permit(:stop_name, :lat, :long, :user_id)
+		params.require(:stop).permit(:stop_name, :lat, :long)
 	end
 end
